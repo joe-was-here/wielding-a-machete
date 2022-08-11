5695a978727c751a4aff57e99a42ef040d900fbf
Joe Madden <joe@nayya.com>
2022-08-11 15:38:24 -0400

    feat(greeting): input for user name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,20 +4,37 @@ import "./App.css";
 
 const App = () => {
   const [greeting, setGreeting] = useState("");
+  const [name, setName] = useState("");
+
+  const getGreeting = async () => {
+    const response = await axios.get("http://localhost:5555/greeting", {
+      params: { name },
+    });
+    setGreeting(response.data);
+  };
 
   useEffect(() => {
-    console.log("App rendered");
-    axios
-      .get("http://localhost:5555/greeting", { params: { name: "joe" } })
-      .then((res) => {
-        console.log(res.data);
-        setGreeting(res.data);
-      });
+    getGreeting();
   }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    getGreeting();
+  };
 
   return (
     <div className="App">
       <header className="App-header">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
         <h1>{greeting}</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
